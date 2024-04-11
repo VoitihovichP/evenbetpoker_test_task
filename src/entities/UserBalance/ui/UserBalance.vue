fc63575e73a90fc4a8d7eddc66330b2dbc8d6fed
Pavel Voitekhovich <pasha2003228@gmail.com>
2024-04-12 00:41:01 +0300

    feat: add some widgets

---
+++ src/entities/UserBalance/ui/UserBalance.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+interface IUserBalanceProps {
+  balanceName: string
+  balance: string
+}
+
+const { balanceName, balance } = defineProps<IUserBalanceProps>()
+</script>
+
+<template>
+  <div>
+    <p>{{ balanceName }}</p>
+    <p>{{ balance }}</p>
+  </div>
+</template>
---
+++ src/widgets/SidebarWidget/ui/SidebarWidget.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <aside class="sidebar">sidebar</aside>
+</template>
+
+<style lang="scss" scoped>
+@import 'Styles/vars';
+
+.sidebar {
+  background: $grey;
+}
+</style>
