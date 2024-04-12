2c1316546613213735899d9e9229ba791d3ae2ad
Pavel Voitekhovich <pasha2003228@gmail.com>
2024-04-12 19:25:20 +0300

    feat: add use badge

---
+++ src/entities/UserBadge/ui/UserBadge.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { UserOutlined } from '@ant-design/icons-vue'
+</script>
+
+<template>
+  <div class="user-badge">
+    <UserOutlined />
+    <p class="user-badge__name">User_test_1</p>
+  </div>
+</template>
+
+<style lang="scss">
+@import 'Styles/vars';
+
+.user-badge {
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  background: $dark-blue;
+  border-radius: $br-medium;
+  box-shadow: $card-shadow;
+  &__name {
+    color: $white;
+    font-size: 20px;
+    font-weight: 400;
+  }
+  .anticon-user {
+    svg {
+      fill: $yellow;
+      width: 4em !important;
+      height: 4em !important;
+    }
+  }
+}
+</style>
--- src/widgets/SidebarWidget/ui/SidebarWidget.vue
+++ src/widgets/SidebarWidget/ui/SidebarWidget.vue
@@ -1,7 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import UserBadge from '@/entities/UserBadge/ui/UserBadge.vue'
+</script>
 
 <template>
-  <aside class="sidebar">sidebar</aside>
+  <aside class="sidebar">
+    <UserBadge />
+  </aside>
 </template>
 
 <style lang="scss" scoped>
@@ -10,6 +14,7 @@
 .sidebar {
   position: fixed;
   left: 0;
+  padding: 12px 18px;
   height: 100dvh;
   width: $sidebar-width;
   background: $grey;
